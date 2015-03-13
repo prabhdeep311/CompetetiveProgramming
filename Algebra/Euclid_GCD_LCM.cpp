c0d94d41bff9cd499814a0c1ca75bc10aefd84a7
Prabhdeep <prabhdeep3114@gmail.com>
2015-03-13 18:54:50 +0530

    Calculate GCD and LCM

---
+++ Algebra/Euclid_GCD_LCM.cpp
@@ -0,0 +1,28 @@
+//
+//  Euclid_GCD_LCM.cpp
+//
+//  Created by Prabhdeep Singh Walia on 13/03/15.
+//  Copyright (c) 2015 Prabhdeep Singh Walia. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return b ? gcd(b, b%a) : a;
+}
+
+int lcm(int a, int b)
+{
+    return (a*b)/gcd(a, b);
+}
+
+int main(int argc, const char * argv[])
+{
+    int a, b;
+    cin>>a>>b;
+    cout<<gcd(a, b)<<"\n"<<lcm(a, b)<<"\n";
+    return 0;
+}
+
